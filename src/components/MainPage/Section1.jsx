755034f15337a7e4d43d820f920afd7bafdb0ff6
Doyeon Kim <nsa10050@naver.com>
2025-05-23 16:47:41 +0900

    d121

--- src/components/MainPage/Section1.jsx
+++ src/components/MainPage/Section1.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { instance } from '../../api/sanrio';
 import '../../styles/main.scss';
+import axios from 'axios';
 
 function Section1() {
     const BASE_URL = `${process.env.REACT_APP_APIURL_IMG}/`;
