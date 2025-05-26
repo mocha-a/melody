f3fde7b9a86d83012d028aff8ec2262858ace117
Doyeon Kim <nsa10050@naver.com>
2025-05-26 11:08:42 +0900

    되어라

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,7 +10,6 @@ import Facebook from '../components/icon/Facebook';
 import Insta from '../components/icon/Insta';
 import Twitter from '../components/icon/Twitter';
 
-
 function MainPage() {
   const [logoutModal, setLogoutModal] = useState(false);
   const [loginModal, setLoginModal] = useState(false);
@@ -20,87 +19,82 @@ function MainPage() {
     const timer = setTimeout(() => {
       setShow(false);
     }, 1000);
-
     return () => clearTimeout(timer);
   }, []);
 
-
-  // 세션에 로그인/로그아웃 모달 상태 저장
   useEffect(() => {
-      if (sessionStorage.getItem("logout_notice")) {
-        setLogoutModal(true);
-        setTimeout(() => {
-          setLogoutModal(false);
-          sessionStorage.removeItem("logout_notice");
-        }, 1000);
-      }
+    if (sessionStorage.getItem("logout_notice")) {
+      setLogoutModal(true);
+      setTimeout(() => {
+        setLogoutModal(false);
+        sessionStorage.removeItem("logout_notice");
+      }, 1000);
+    }
 
-      if (sessionStorage.getItem("login_success")) {
-        setLoginModal(true);
-        setTimeout(() => {
-          setLoginModal(false);
-          sessionStorage.removeItem("login_success");
-        }, 1000);
-      }
-    }, []);
+    if (sessionStorage.getItem("login_success")) {
+      setLoginModal(true);
+      setTimeout(() => {
+        setLoginModal(false);
+        sessionStorage.removeItem("login_success");
+      }, 1000);
+    }
+  }, []);
 
-return (
-    <>
-      {show ? (
-        <div className="list_loading">
-          <CircularColor />
-        </div>
-      ) : (
-        <div className="main_container">
-          {/* 로그인/로그아웃 모달창 */}
-          {logoutModal && (
-            <div className="modal">
-              <div className="alert_box">로그아웃 되었습니다.</div>
-            </div>
-          )}
+  if (show) {
+    return (
+      <div className="list_loading">
+        <CircularColor />
+      </div>
+    );
+  }
 
-          {loginModal && (
-            <div className="modal">
-              <div className="alert_box">로그인 되었습니다.</div>
-            </div>
-          )}
-
-          <section>
-            <Section1 />
-            <Marquee1 />
-            <Section2 />
-            <Marquee2 />
-          </section>
-
-            <div className='footer_container'>
-              <div className='footer_content'>
-                  <div className="footer_snsIcon">
-                      <Facebook />
-                      <Twitter />
-                      <Insta />
-                  </div>
-
-                  <div className="footer_text">
-                      <div className='footer_text_title'>
-                          <p>멜로디</p>
-                          <SolidLine className="footer_solid"/>
-                      </div>
-
-                      <div className='footer_text_title'>
-                          <p>
-                              대표 : 안지현, 김도연{"\n"}
-                              서울특별시 강남구 강남대로98길 16{"\n"}
-                              오전 09시 ~ 오후 18시 (주말, 공휴일 제외){"\n"}
-                              melody0123@gmail.com
-                          </p>
-                      </div>
-                  </div>
-
-              </div>
-          </div>
+  return (
+    <div className="main_container">
+      {/* 로그인/로그아웃 모달창 */}
+      {logoutModal && (
+        <div className="modal">
+          <div className="alert_box">로그아웃 되었습니다.</div>
         </div>
       )}
-    </>
+      {loginModal && (
+        <div className="modal">
+          <div className="alert_box">로그인 되었습니다.</div>
+        </div>
+      )}
+
+      <section>
+        <Section1 />
+        <Marquee1 />
+        <Section2 />
+        <Marquee2 />
+      </section>
+
+      <div className="footer_container">
+        <div className="footer_content">
+          <div className="footer_snsIcon">
+            <Facebook />
+            <Twitter />
+            <Insta />
+          </div>
+
+          <div className="footer_text">
+            <div className="footer_text_title">
+              <p>멜로디</p>
+              <SolidLine className="footer_solid" />
+            </div>
+
+            <div className="footer_text_title">
+              <p>
+                대표 : 안지현, 김도연<br />
+                서울특별시 강남구 강남대로98길 16<br />
+                오전 09시 ~ 오후 18시 (주말, 공휴일 제외)<br />
+                melody0123@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
